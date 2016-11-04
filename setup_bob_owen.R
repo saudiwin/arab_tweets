7ac00cbdb077e984e648b58096fc6590b4535a28
Chen Wang <Chen Wang>
2016-11-04 11:07:17 -0400

    setting up project

---
+++ setup_bob_owen.R
@@ -0,0 +1,12 @@
+install.packages("RPostgreSQL")
+require(RPostgreSQL)
+
+conn <- dbConnect(dbDriver('PostgreSQL'),dbname='alltweets',
+                  host='arabtweets.cytudbr9ljth.us-west-2.rds.amazonaws.com',
+                  user='uva_tweet',
+                  password='A3abT7eets')
+
+user_ids<- dbGetQuery(conn,'SELECt unique_id FROM users;')
+
+#tweet_ids<- dbGetQuery()
+
