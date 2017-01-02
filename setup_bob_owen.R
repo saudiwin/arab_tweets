03541cd0efff3937dd8c99a0fddcaaa35139fa9a
bob_kubinec <rmk7xy@virginia.edu>
2017-01-02 10:52:41 -0500

    have rtweet package working

--- setup_bob_owen.R
+++ setup_bob_owen.R
@@ -1,14 +1,21 @@
-install.packages("RPostgreSQL")
+#install.packages("RPostgreSQL")
 require(RPostgreSQL)
-
+require(dplyr)
+require(rtweet)
 conn <- dbConnect(dbDriver('PostgreSQL'),dbname='alltweets',
                   host='arabtweets.cytudbr9ljth.us-west-2.rds.amazonaws.com',
                   user='uva_tweet',
                   password='A3abT7eets')
 
-user_ids<- dbGetQuery(conn,'SELECt unique_id FROM users;')
+user_ids<- dbGetQuery(conn,'SELECt username FROM users LIMIT 100;')
 
-#tweet_ids<- dbGetQuery()
+twitter_token <- create_token(app = "For Analysis II", # whatever you named app
+                              consumer_key = "6ae7rNvww5q13Kc86d8YvvLZG",
+                              consumer_secret = "gfZOqDIGpzkr53UXHrsqgzWRwIiqlerm982NWPcxP1hIf2uwuV")
 
-# What is the purpose of this file?
 
+# Loop over people and get tweets
+all_tweets <- lapply(user_ids$username,function(x) {
+    y <- get_timeline(x)
+    return(y)
+})
