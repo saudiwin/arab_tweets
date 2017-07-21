cf7932dfb426ec219dd4fbcbc8fe5b6daafcff20
saudiwin <bobkubinec@gmail.com>
2017-07-21 15:22:00 -0400

    overlapp

--- create_json_gnip.R
+++ create_json_gnip.R
@@ -11,12 +11,12 @@ elites <- read_csv('data/cairo_elites_all.csv')
 to_gnip <- select(elites,just_unique) %>% 
   rename(from='just_unique') %>% 
   mutate(retweets_of=from)
-over_rows <- rep(1:3,each=ceiling(nrow(to_gnip)/3))[1:nrow(to_gnip)]
+over_rows <- rep(1:4,each=ceiling(nrow(to_gnip)/4))[1:nrow(to_gnip)]
 
 for(i in 1:5) {
   this_gnip <- filter(elites,over_rows==i)
   cat(paste0('from: ',
-             paste0(this_gnip$just_unique,collapse = ' OR ')),
+             paste0(this_gnip$just_unique,collapse = ' OR from: ')),
       file=paste0('data/to_gnip_from_',i,'.txt'),
       append=F)
 }
@@ -24,7 +24,7 @@ for(i in 1:5) {
 for(i in 1:5) {
   this_gnip <- filter(elites,over_rows==i)
   cat(paste0('retweets_of: ',
-             paste0(this_gnip$just_unique,collapse = ' OR ')),
+             paste0(this_gnip$just_unique,collapse = ' OR from: ')),
       file=paste0('data/to_gnip_retweets_',i,'.txt'),
       append=F)
 }
