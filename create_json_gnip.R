a7e705dc917b4a6f2d7a79237380c3da1208abab
bob_kubinec <rmk7xy@virginia.edu>
2017-07-21 15:11:49 -0400

    ready to process new tweets

--- create_json_gnip.R
+++ create_json_gnip.R
@@ -6,7 +6,8 @@ require(tidyr)
 require(readr)
 
 
-elites <- read_csv('data/cairo_elites_all.csv')
+elites <- read_csv('data/egypt_and_tunis_to_gnip.csv.csv') %>% 
+  filter(`Keep?`=='Yes' | is.na(`Keep?`))
 
 to_gnip <- select(elites,just_unique) %>% 
   rename(from='just_unique') %>% 
@@ -16,7 +17,7 @@ over_rows <- rep(1:3,each=ceiling(nrow(to_gnip)/3))[1:nrow(to_gnip)]
 for(i in 1:5) {
   this_gnip <- filter(elites,over_rows==i)
   cat(paste0('from: ',
-             paste0(this_gnip$just_unique,collapse = ' OR ')),
+             paste0(this_gnip$just_unique,collapse = ' OR from: ')),
       file=paste0('data/to_gnip_from_',i,'.txt'),
       append=F)
 }
--- pick_new_users.R
+++ pick_new_users.R
@@ -7,26 +7,26 @@ require(readr)
 require(ggplot2)
 # load tables
 
-all_cairo <- dbConnect(SQLite(),'data/all_cairo.sqlite')
+all_cairo <- dbConnect(SQLite(),'data/all_tunis.sqlite')
 current_users <- dbConnect(SQLite(),'data/user.sqlite')
-all_ment <- readRDS('cairo_all_ment.rds') %>% 
+all_ment <- readRDS('tunis_all_ment.rds') %>% 
   mutate(just_unique=str_replace(just_unique,'@','')) 
 
-all_users <- dbGetQuery(current_users,'SELECT DISTINCT username from users;') %>% 
+all_users <- dbGetQuery(current_users,"SELECT DISTINCT username from users WHERE location LIKE '%tunis%';") %>% 
   mutate(username=tolower(username))
 
-new_ids <- read_csv('data/need_users_cairo.csv - need_users_cairo.csv (1).csv')
+new_ids <- read_csv('need_users_tunis.csv')
 
 existing_ids <- filter(all_ment,just_unique %in% all_users$username) 
-keep_new_ids <- filter(new_ids,Include=='Yes')
+#keep_new_ids <- filter(new_ids,Include=='Yes')
 
-all_gnip <- bind_rows(keep_new_ids,existing_ids) %>% arrange(desc(t_ment))
-elites <- filter(all_gnip,t_users>1000)
-filter(all_gnip,t_users>1000) %>% 
+all_gnip <- bind_rows(new_ids,existing_ids) %>% arrange(desc(t_ment))
+elites <- filter(all_gnip,t_ment>1000)
+filter(all_gnip,t_ment>1000) %>% 
   ggplot() +
   geom_histogram(aes(x=t_ment)) +
   theme_minimal()
 
 
-select(elites,just_unique) %>% write_csv('cairo_elites.csv')
-write_csv(elites,'cairo_elites_all.csv')
+select(elites,just_unique) %>% write_csv('tunis_elites.csv')
+write_csv(elites,'tunis_elites_all.csv')
