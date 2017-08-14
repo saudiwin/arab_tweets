54fbfbb1058700e15db5dda88f7ce32aae394849
saudiwin <bobkubinec@gmail.com>
2017-08-14 14:31:06 -0400

    combining json files

---
+++ process_json.R
@@ -0,0 +1,45 @@
+#need to process JSON files
+
+
+require(dplyr)
+require(tidyr)
+require(readr)
+require(forcats)
+require(RSQLite)
+
+tunis_sql <- dbConnect(SQLite(),'data/tunis_tweets.sqlite')
+egypt_sql <- dbConnect(SQLite(),'data/egypt_tweets.sqlite')
+all_files <- list.files('PTDataDownload/downloads/',full.names = T)
+
+country_list <- read_csv('data/Final Coding -- Dana - Sheet1.csv')
+
+tunisia <- filter(country_list,Country=='Tunisia') %>% pull(Username)
+egypt <- filter(country_list,Country=='Egypt') %>% pull(Username)
+
+data1 <- jsonlite::stream_in(file('PTDataDownload/20130331-20131231_hzedzpn45d_2013_10_03_22_40_activities.json'))
+data2 <- ndjson::stream_in('PTDataDownload/20130331-20131231_hzedzpn45d_2013_10_03_22_40_activities.json')
+
+tunisia <- lapply(all_files,function(f) {
+  data1 <- ndjson::stream_in('f')
+  data1 <- select(data2,actor.displayName,actor.followersCount,
+                  actor.location.displayName,
+                  actor.postedTime,
+                  actor.statusesCount,
+                  actor.summary,
+                  actor.twitterTimeZone,
+                  actor.utcOffset,
+                  actor.preferredUsername,
+                  actor.id,
+                  body,
+                  favoritesCount,
+                  id,
+                  object.postedTime,
+                  retweetCount,
+                  gnip.urls.0.expanded_url)
+  return(data1)
+}) %>% bind_rows
+
+just_tunis <- filter(tunisia,)
+
+dbDisconnect(tunis_sql)
+dbDisconnect(egypt_sql)
