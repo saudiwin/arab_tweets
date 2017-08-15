8817f37f653d7b071719b7da5585e6907ebee3e5
saudiwin <bobkubinec@gmail.com>
2017-08-14 20:24:16 -0400

    finished downloading json files

--- process_json.R
+++ process_json.R
@@ -20,26 +20,28 @@ data1 <- jsonlite::stream_in(file('PTDataDownload/20130331-20131231_hzedzpn45d_2
 data2 <- ndjson::stream_in('PTDataDownload/20130331-20131231_hzedzpn45d_2013_10_03_22_40_activities.json')
 
 tunisia <- lapply(all_files,function(f) {
-  data1 <- ndjson::stream_in('f')
-  data1 <- select(data2,actor.displayName,actor.followersCount,
-                  actor.location.displayName,
-                  actor.postedTime,
-                  actor.statusesCount,
-                  actor.summary,
-                  actor.twitterTimeZone,
-                  actor.utcOffset,
-                  actor.preferredUsername,
-                  actor.id,
-                  body,
-                  favoritesCount,
-                  id,
-                  object.postedTime,
-                  retweetCount,
-                  gnip.urls.0.expanded_url)
+  data1 <- ndjson::stream_in(f)
+  all_names <- c('actor.displayName','actor.followersCount',
+                 'actor.location.displayName',
+                 'actor.postedTime',
+                 'actor.statusesCount',
+                 'actor.summary',
+                 'actor.twitterTimeZone',
+                 'actor.utcOffset',
+                 'actor.preferredUsername',
+                 'actor.id',
+                 'body',
+                 'favoritesCount',
+                 'id',
+                 'object.postedTime',
+                 'retweetCount',
+                 'gnip.urls.0.expanded_url')
+  exist_names <- all_names[all_names %in% names(data1)]
+  data1 <- select(data1,one_of(exist_names))
   return(data1)
 }) %>% bind_rows
 
-just_tunis <- filter(tunisia,)
+saveRDS(tunisia,'data/all_elites_tweets.rds')
 
 dbDisconnect(tunis_sql)
 dbDisconnect(egypt_sql)
