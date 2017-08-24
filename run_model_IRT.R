e9d073a2eee399e19780607d582de80d8fb4bae9
saudiwin <bobkubinec@gmail.com>
2017-08-24 11:46:09 -0400

    running full models

--- run_model_IRT.R
+++ run_model_IRT.R
@@ -62,28 +62,45 @@ combined_data <- bind_rows(filter(egypt_rts,rt_ids %in% filter_egypt$rt_ids),
                            filter(tunis_rts,rt_ids %in% filter_tunis$rt_ids)) %>% 
   mutate(username=tolower(username))
 
+# need to make the coup indicator & change time to three day intervals
+coup_day <- lubridate::yday('2013-07-03')
 old_days <- min(combined_data$time):max(combined_data$time)
 seq_val <- floor(length(old_days)/3)
 new_days <- rep(1:seq_val,each=3)
 if(length(new_days)<length(old_days)) {
   new_days <- c(new_days,rep(seq_val+1,times=length(old_days)-length(new_days)))
 }
-
+coup_day_new <- new_days[which(old_days>coup_day & (old_days %% 3))[1]]
+times <- data_frame(time=old_days,time_three=new_days,
+                    coup=if_else(time_three>coup_day_new,2L,1L),
+                    coup_day=coup_day)
 combined_data <- left_join(combined_data,
-                           data_frame(time=old_days,time_three=new_days))
+                           times)
+
+times <- distinct(times,time_three,coup)
 
 combined_data_small <- group_by(combined_data,
+                                coup,
+                                time_three,
                                 username,
                                 rt_ids) %>% count
 
+
+
 # add in codings
 
-combined_data_small <- left_join(combined_data,elite_coding,
-                                 by=c('username'='Username'))
+combined_data_small <- ungroup(combined_data_small) %>% left_join(elite_coding,
+                                 by=c('username'='Username')) %>% 
+  mutate(user_ids=as.numeric(factor(username)),
+         cit_ids=as.numeric(factor(rt_ids)))
+
+# drop missing
+
+combined_data_small_nomis <- filter(combined_data_small,!is.na(coding_num))
 
 start_func <- function() {
-  list(alpha=rbind(matrix(c(init_sides1,init_sides2),ncol=4),
-                   matrix(rep(0, 99*4),ncol=4)),
+  list(alpha=rbind(matrix(c(-1,-1,1,1),ncol=4),
+                   matrix(rep(0, (max(combined_data_small_nomis$time_three)-1)*4),ncol=4)),
        gamma1=c(0.5,0.5),
        gamma2=c(0.5,0.5),
        ts_sigma=rep(0.25,4),
@@ -92,26 +109,100 @@ start_func <- function() {
        mean_beta=0,
        sigma_beta=1,
        sigma_delta=1,
-       beta=rnorm(n=100),
-       delta=rnorm(100),
+       beta=rnorm(max(combined_data_small_nomis$cit_ids)),
+       delta=rnorm(max(combined_data_small_nomis$cit_ids)),
        gamma_par1=0,
        gamma_par2=0)
 }
 
-out_fit <- sampling(code_compile,
-                    data=list(J=sides,
-                              K=cit,
-                              `T`=t,
-                              N=length(gen_out),
-                              jj=combine_ids[,1],
-                              kk=combine_ids[,2],
-                              tt=combine_ids[,3],
-                              y=as.integer(gen_out),
-                              coup=as.integer(t/2),
-                              start_vals=c(init_sides1,init_sides2),
-                              time_gamma=time_gamma),
-                    cores=4,
-                    control=list(max_treedepth=11,
-                                 adapt_delta=0.9),
+code_compile <- stan_model(file='ord_irt_v1.stan')
+
+out_fit <- vb(code_compile,
+                    data=list(J=max(combined_data_small_nomis$coding_num),
+                              K=max(combined_data_small_nomis$cit_ids),
+                              `T`=max(combined_data_small_nomis$time_three),
+                              N=nrow(combined_data_small_nomis),
+                              C=3,
+                              jj=combined_data_small_nomis$coding_num,
+                              kk=combined_data_small_nomis$cit_ids,
+                              tt=combined_data_small_nomis$time_three,
+                              y=as.integer(combined_data_small_nomis$nn),
+                              coup=as.integer(floor(max(combined_data_small_nomis$time_three)/2)),
+                              start_vals=c(-1,-1,1,1),
+                              time_gamma=times$coup[-nrow(times)]),
                     init=start_func)
 to_plot <- as.array(out_fit)
+
+mcmc_intervals(to_plot,regex_pars = 'adj')
+mcmc_trace(to_plot,pars='gamma1[1]')
+mcmc_trace(to_plot,pars='gamma1[2]')
+mcmc_trace(to_plot,pars='gamma2[1]')
+mcmc_trace(to_plot,pars='gamma2[2]')
+mcmc_intervals(to_plot,regex_pars = c('gamma1|gamma2'))
+mcmc_intervals(to_plot,regex_pars = c('alpha'))
+
+gamma1 <- rstan::extract(out_fit,pars='gamma1')$gamma1
+
+gamma2 <- rstan::extract(out_fit,pars='gamma2')$gamma2
+
+all_gammas <- data_frame(Islamists=gamma1[,2]-gamma1[,1],
+                         Secularists=gamma2[,2]-gamma2[,1]) %>% 
+  gather(`Ideological\nPairing`,Difference) %>% 
+  group_by(`Ideological\nPairing`) %>% 
+  mutate(mean_val=mean(Difference))
+
+ggplot(all_gammas,aes(x=Difference)) +
+  geom_density(aes(fill=`Ideological\nPairing`),colour=NA,alpha=0.5,adjust=0.5) +
+  theme_minimal() +
+  theme(panel.grid = element_blank()) +
+  xlab('Gamma Difference') +
+  ylab('Posterior Density') +
+  geom_vline(aes(xintercept=mean_val,linetype=`Ideological\nPairing`))
+
+get_time <- rstan::extract(out_fit,pars='alpha',permute=T)$alpha
+get_time <- get_time[sample(1:nrow(get_time),101),,]
+get_time <- lapply(1:dim(get_time)[3],function(x) get_time[,,x]) %>% 
+  lapply(as_data_frame) %>% 
+  bind_rows(.id='Series') %>% 
+  mutate(Series=factor(Series),
+         Series=fct_recode(Series,`Tunisia Islamists`='1',
+                           `Egyptian Islamists`='2',
+                           `Tunisian Secularists`='3',
+                           `Egyptian Secularists`='4')) %>% 
+  gather(time_pts,out_vals,-Series) %>% 
+  mutate(time_pts=as.numeric(factor(time_pts)))
+
+return_cl <- function(var_out) {
+  data_frame()
+}
+
+get_time %>% 
+  ggplot(aes(y=out_vals,x=time_pts)) +
+  stat_smooth() + theme_minimal() +
+  theme(panel.grid=element_blank()) + xlab('Time') + ylab('Ideological Positions') + 
+  scale_colour_brewer(palette='paired',name='') + 
+  facet_wrap(~Series) +
+  scale_linetype(name='')
+
+get_time %>% 
+  ggplot(aes(y=out_vals,x=time_pts)) +
+  stat_summary(geom='ribbon',fun.data = 'median_hilow',fill='grey80') + theme_minimal() +
+  stat_summary(fun.y='median',geom='path',linetype=2) +
+  theme(panel.grid=element_blank()) + xlab('Time') + ylab('Ideological Positions') + 
+  scale_colour_brewer(palette='paired',name='') + 
+  facet_wrap(~Series) +
+  scale_linetype(name='') + 
+  geom_vline(aes(xintercept=32),linetype=3)
+
+ggsave('arab_ideology.png')
+
+
+get_time %>% 
+  ggplot(aes(y=out_vals,x=time_pts)) +
+  stat_summary(geom='ribbon',fun.data = 'median_hilow') + theme_minimal() +
+  theme(panel.grid=element_blank()) + xlab('Time') + ylab('Ideological Positions') + 
+  scale_colour_brewer(palette='paired',name='') + 
+  facet_wrap(~Series) +
+  scale_linetype(name='')
+
+
